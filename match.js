727c30a31afa9a8bb7fcc2a50a694a417586a1f7
Dennis Hou <dennis.hou@rakuten.com>
2023-04-13 10:27:56 +1000

    Fixed up package.json and added further clarifications on requirements

--- match.js
+++ match.js
@@ -1,5 +1,5 @@
 module.exports.match = function(hash) {
 
-  // Write your solution to Task #2 - Match Metadata task here
+  // Write your solution to Task #3 - Match Credit Card Controls task here
   
 }
